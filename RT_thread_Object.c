dec72e4249f1fbab9c99e8762fdfd934d017452d
hemeng <hemeng@fuerda-china.com>
2024-05-28 13:48:34 +0800

    rt_thread_object update

--- RT_thread_Object.c
+++ RT_thread_Object.c
@@ -1,8 +1,15 @@
+/*
+ *   文件名: FILE NAME
+ *   作者:    -
+ *   日期:    -
+ *   版本:    -
+ *   说明:    -
+ *   修改记录: -
+ */
 
 
 //1.函数调用方式：
 typedef int (*init_fn_t)(void);
-
 #define SECTION(x)                  __attribute__((section(x)))
 #define RT_USED                     __attribute__((used))
 
@@ -10,8 +17,25 @@ typedef int (*init_fn_t)(void);
 INIT_BOARD_EXPORT(uart_init);
 #define INIT_BOARD_EXPORT(fn)        INIT_EXPORT(fn, 	"1")
 #define INIT_EXPORT(fn, level)       RT_USED SECTION(".rti_fn.init") const init_fn_t __rt_init_##fn  = fn
-																	 const init_fn_t __rt_init_uart_init = uart_init
+															 const init_fn_t __rt_init_uart_init = uart_init
+xx.map .o文件链接顺序,按照链接文件统一顺序链接； 
+指针保存在flash 某个地址中,地址中保存的值为函数地址；          
+
+
 //2.启动流程
+加载域与执行域：
+    2.1.重定位 +RW 数据段
+    2.2.+ZI
+    2.3.调用__main->$sub$$main->($sup$$main()->)rtthread_startup()
+        单板的初始化
+        显示rt_thread 版本
+        定时器初始化
+        调度器初始化
+        application_init->(main只是在某个线程的函数里面被调用而已);
+
+
+
+
 //3.rt_object:
 struct rt_object
 {
